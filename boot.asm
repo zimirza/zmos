4270c08d282f9f42582d280699096263f34651ee
zishan <zmirza@posteo.ch>
2024-07-14 14:47:42 +0200

    set window and cursor

--- boot.asm
+++ boot.asm
@@ -4,6 +4,17 @@ ORG 0x7C00
 JMP _start
 
 _start:
+  MOV AH, 0x0006
+  MOV BH, 0x001F
+  MOV DX, 0x184F
+  INT 0x0010
+
+  MOV AH, 0x0002
+  MOV BH, 0x0000
+  MOV DH, 0x0000
+  MOV DL, 0x0000
+  INT 0x0010
+
   JMP $
 
 TIMES 510-($-$$) DB 0

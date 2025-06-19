81b17d790cdba6863b8170a5dcdc5835d95cb5a8
zishan <zmirza@posteo.ch>
2025-06-19 18:48:27 +0200

    refactor: move print function to different file

--- boot.asm
+++ boot.asm
@@ -3,27 +3,7 @@ ORG 0x7C00
 
 JMP _start
 
-print_new_line:
-  MOV AH, 0x0002
-  MOV BH, 0x0000
-  INC DH
-  INT 0x0010
-
-print:
-  MOV AH, 0x000E
-  MOV AL, 0x0000
-  MOV BL, 0x0007
-
-print_loop:
-  MOV AL, [SI]
-  CMP AL, 0x0000
-  JZ print_done
-  INC SI
-  INT 0x0010
-  JMP print_loop
-
-print_done:
-  RET
+%include "print.S"
 
 _start:
   MOV AH, 0x0006
